b2847349aa328f84a56c8f55d6c90027a821e899
Hila Etziony <hilaetziony@mail.tau.ac.il>
2025-11-17 14:13:32 +0000

    few extra things in AST

--- ex2/ex2-main/src/ast/AstStmtIf.java
+++ ex2/ex2-main/src/ast/AstStmtIf.java
@@ -4,13 +4,20 @@ public class AstStmtIf extends AstStmt
 {
 	public AstExp cond;
 	public AstStmtList body;
+	public AstStmtList elseBody;
 
 	/*******************/
 	/*  CONSTRUCTOR(S) */
 	/*******************/
 	public AstStmtIf(AstExp cond, AstStmtList body)
 	{
+		this(cond, body, null);
+	}
+
+	public AstStmtIf(AstExp cond, AstStmtList body, AstStmtList elseBody)
+	{
 		this.cond = cond;
 		this.body = body;
+		this.elseBody = elseBody;
 	}
 }
---
+++ ex2/ex2-main/src/ast/AstStmtReturn.java
@@ -0,0 +1,14 @@
+package ast;
+
+public class AstStmtReturn extends AstStmt
+{
+	public AstExp exp;
+
+	/*******************/
+	/*  CONSTRUCTOR(S) */
+	/*******************/
+	public AstStmtReturn(AstExp exp)
+	{
+		this.exp = exp;
+	}
+}
