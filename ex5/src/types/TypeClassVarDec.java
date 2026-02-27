b6b4cbcd5e98f2050cbab18931a0c54b49304e89
Hila Etziony <hilaetziony@mail.tau.ac.il>
2026-02-27 12:58:25 +0000

    fix test 5 8 9 - initialize field of class (IR ISSUE)

--- ex5/src/types/TypeClassVarDec.java
+++ ex5/src/types/TypeClassVarDec.java
@@ -1,15 +1,19 @@
 package types;
 
+import ast.AstExp;
+
 public class TypeClassVarDec extends Type
 {
 	public Type t;
+	public AstExp initValue;
 	// Note: 'name' and 'offset' are inherited from Type
 
-	public TypeClassVarDec(Type t, String name, int offset)
+	public TypeClassVarDec(Type t, String name, int offset, AstExp initValue)
 	{
 		this.t = t;
-		this.name = name;     // sets Type.name (inherited)
-		this.offset = offset; // sets Type.offset (inherited)
+		this.name = name;
+		this.offset = offset;
+		this.initValue = initValue;
 	}
 
 }
